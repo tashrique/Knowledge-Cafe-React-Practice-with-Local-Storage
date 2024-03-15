39771dc8e6332871c4553f0e8303870d648d58b7
Tashrique Ahmed <ta13@williams.edu>
2024-03-15 06:55:49 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Header from './assets/components/Header/Header'
+
+function App() {
+
+  return (
+    <>
+      <div className="">
+        <Header></Header>
+
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
